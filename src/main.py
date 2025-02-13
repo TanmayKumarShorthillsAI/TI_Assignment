8725fea840e7cc9b7a194fcec84aea508a27138e
TanmayKumarShorthillsAI <tanmay@shorthills.ai>
2025-02-13 11:26:09 +0530

    TICKET: 10192: Restructured and extrcated information such as Dealer_Id, Ad_Id and impression count

--- src/main.py
+++ src/main.py
@@ -1,4 +1,4 @@
-from extract import LoadDataframe;
+from extract import LoadDataframe
 from transform import TransformData
 import pandas as pd
 
@@ -7,13 +7,13 @@ def main():
     load_dataframe = LoadDataframe("../assets/")
     combined_df = load_dataframe.make_df()
     transform_dataframe = TransformData(combined_df)
+
     transform_dataframe.drop_and_rename_columns()
-    event_id = '20113'
-    event_id_filtered_df = transform_dataframe.filter_based_on_event_id(event_id=event_id)
-    # print(event_id_filtered_df.head())
-    # print(event_id_filtered_df.iloc[11]["Post_product_list"])
-    transform_dataframe.expand_product_list(event_id_filtered_df)
-    
+    event_id = "20113"
+    transform_dataframe.filter_based_on_event_id(event_id=event_id)
+    transform_dataframe.expand_product_list()
+    transform_dataframe.produce_product_info_df()
+
 
 if __name__ == "__main__":
     main()
--- src/transform.py
+++ src/transform.py
@@ -3,6 +3,7 @@ import pandas as pd
 class TransformData():
     def __init__(self, df):
         self.df = df
+        self.product_list = []
         pd.set_option('display.max_columns', 10)
         pd.set_option('display.width', 500)
 
@@ -11,16 +12,41 @@ class TransformData():
 
         self.df = self.df.rename(columns = {0:'Date', 1:'Post_event_list', 2:'Post_product_list', 4:'URL'})
         self.df.fillna('', inplace=True)
+        # print(self.df.head())
 
 
-    def filter_based_on_event_id(self, event_id) -> pd.DataFrame:
-        event_filtered_df = self.df[self.df['Post_event_list'].apply(lambda x: event_id in x.split(','))]
+    def filter_based_on_event_id(self, event_id):
+        self.df = self.df[self.df['Post_event_list'].apply(lambda x: event_id in x.split(','))]
         
-        return event_filtered_df
+        # return event_filtered_df
     
-    def expand_product_list(self, event_filtered_df):
-        post_product_list = event_filtered_df["Post_product_list"].apply(lambda x: x.split(';;;'))
-        product_list_df = post_product_list.explode()
-        print(product_list_df.head())
+    def expand_product_list(self):
+        self.df["Post_product_list"] = self.df["Post_product_list"].apply(lambda x: x.split(','))
+        self.df = self.df.explode(column=["Post_product_list"]).filter(["Post_product_list", "URL"])
+
+    def make_new_records(self, arr, url):
+        product_dict = {"Dealer_Id":'', "Ad_Id": '', "Impression_count": '', 'Domain_name':'', "Product_data": ''}
+
+        try:
+            product_dict['Domain_name'] = url.split('/')[2].split('.')[1]
+            
+            product_dict['Dealer_Id'] = arr[0]
+            product_dict['Ad_Id'] = arr[1]
+            product_dict['Impression_count'] = arr[4].split('|')[0].split('=')[-1]
+            product_dict['Product_data'] = "|".join(arr[4].split('|')[1:]) + ";" + arr[5:]
+
+            self.product_list.append(product_dict)
+        except Exception as e:
+            print(e)
+
+
+
+    def produce_product_info_df(self):
+        self.df["Post_product_list"] = self.df["Post_product_list"].apply(lambda x: x.split(';'))
+        self.df.apply(lambda x: self.make_new_records(x.Post_product_list, x.URL), axis=1)
+
+        product_info_df = pd.DataFrame(self.product_list)
+        print(product_info_df)
+        
 
         
